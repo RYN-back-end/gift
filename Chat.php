160e6083483f7ea9f29cb89c77097755d1deda8a
mohamed gamal <mohamedgammal55@gmail.com>
2024-04-17 08:20:48 +0200

    17-04

--- Chat.php
+++ Chat.php
@@ -145,7 +145,7 @@ include 'layout/inc/header.php';
             type: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'حطية هنا'
+                'Authorization': 'Bearer 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'
             },
             data: JSON.stringify({
                 'model': 'gpt-3.5-turbo',
